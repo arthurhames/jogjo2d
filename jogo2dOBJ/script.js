e85007ac3548ed25bd8e0509d4bd2d04d1e621db
ARTHUR DA SILVA HAMES <arthur_hames@estudante.sesisenai.org.br>
2025-04-02 12:43:24 -0300

    jogo 2d orientado obj

---
+++ jogo2dOBJ/script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('jogoCanvas')
+const ctx = canvas.getContext('2d')
+
+class Entidade{
+    constructor(x, y, largura, altura, cor){
+        this.x = x;
+        this.y = y;
+        this.largura = largura;
+        this.altura = altura;
+        this.cor = cor
+    }
+    desenhar(){
+        ctx.fillStyle = this.cor
+        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+    }
+}
+class Jogo{
+    constructor(){
+        this.loop = this.loop.bind(this)
+    }
+    loop(entidade){
+        entidade.desenhar()
+        resquestAnimationFrame(this.loop)
+    }
+}
+const entidade = new Entidade(100, 100, 50, 50, 'red')
+const jogo = new Jogo()
+jogo.loop(entidade)
